63f9e5730a10f6984d101d24032f2e944a3ac779
duy <duy@duys-mac.local>
2018-04-14 20:08:50 -0700

    Initial Commit

---
+++ baidemonhom6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  baidemonhom6
+//
+//  Created by duy on 4/14/18.
+//  Copyright © 2018 duy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let a = ("banana")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
